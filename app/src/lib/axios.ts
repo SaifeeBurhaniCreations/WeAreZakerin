b1d97f52b3bbc5357c070ab0c5fdb1aa0fb82961
SaifeeBurhaniCreations <saifeeburhanicreations@gmail.com>
2025-08-13 11:38:15 +0530

    removed extra files

--- app/src/lib/axios.ts
+++ app/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as SecureStore from 'expo-secure-store';
 
-const API_URL = "https://24db-2402-8100-2704-d224-4c7-9d9a-f7bc-fecf.ngrok-free.app/api/v1";
+const API_URL = "https://5a24524ad8a3.ngrok-free.app/api/v1";
 
 const api = axios.create({
   baseURL: API_URL,
